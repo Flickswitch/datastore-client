d350e789941b33997880206c8eddaa4183700724
Jethro Muller <git@jethromuller.co.za>
2019-04-16 12:14:57 +0200

    Move functions into a class

--- datastore_client/client.py
+++ datastore_client/client.py
@@ -5,36 +5,36 @@ from google.cloud.datastore import Entity
 from google.cloud.datastore.query import Iterator
 
 
-client = datastore.Client()
+class DatastoreClient:
+    def __init__(self):
+        self.client = datastore.Client()
 
+    def set_key(self, entity_name: str, key_name: str, **properties: Any) -> None:
+        key = self.client.key(entity_name, key_name)
 
-def set_key(entity_name: str, key_name: str, **properties: Any) -> None:
-    key = client.key(entity_name, key_name)
+        entity = datastore.Entity(key=key)
+        entity.update(properties)
 
-    entity = datastore.Entity(key=key)
-    entity.update(properties)
+        self.client.put(entity)
 
-    client.put(entity)
+    def get_key(self, entity_name: str, key_name: str) -> Optional[Entity]:
+        key = self.client.key(entity_name, key_name)
 
+        return self.client.get(key)
 
-def get_key(entity_name: str, key_name: str) -> Optional[Entity]:
-    key = client.key(entity_name, key_name)
+    def query_entity(
+        self,
+        entity_name: str,
+        *query_filters: Tuple[str, str, Any],
+        projection: List[str]=None,
+        limit: Optional[int]=None,
+    ) -> Iterator:
+        query = self.client.query(kind=entity_name)
 
-    return client.get(key)
+        for query_filter in query_filters:
+            query.add_filter(*query_filter)
 
+        if projection is not None:
+            query.projection = projection
 
-def query_entity(
-    entity_name: str,
-    *query_filters: Tuple[str, str, Any],
-    projection: List[str]=None,
-    limit: Optional[int]=None,
-) -> Iterator:
-    query = client.query(kind=entity_name)
-
-    for query_filter in query_filters:
-        query.add_filter(*query_filter)
-
-    if projection is not None:
-        query.projection = projection
-
-    return query.fetch(limit=limit)
+        return query.fetch(limit=limit)
