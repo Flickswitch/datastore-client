98d2eb9dd02ceefbdab9f00ef9975f5c50deb318
Jethro Muller <git@jethromuller.co.za>
2019-12-05 11:15:42 +0200

    Fix chunking function to actually do it's job

--- datastore_client/utils.py
+++ datastore_client/utils.py
@@ -1,6 +1,17 @@
-from itertools import zip_longest
 
 
 def chunk_iterable(iterable, chunk_size=500):
-    args = [iter(iterable)] * chunk_size
-    return zip_longest(*args)
+    current_chunk_size = 0
+    current_chunk = []
+
+    for item in iterable:
+        current_chunk.append(item)
+        current_chunk_size += 1
+
+        if current_chunk_size == chunk_size:
+            yield current_chunk
+            current_chunk = []
+            current_chunk_size = 0
+
+    if current_chunk:
+        yield current_chunk
